7b3ddf92f976139282c7c6a7d7f86bde7db7cfeb
jaayesta <ja.ayestaran@gmail.com>
2013-02-08 01:13:49 -0200

    Update usuarios/forms.py

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -8,3 +8,19 @@ class UsuarioForm(ModelForm):
 
     class Meta:
         model = Usuario
+        
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UsuarioForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user    
