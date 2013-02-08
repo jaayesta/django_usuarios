eac9b6161b0bb53ff03ab9012a54940d58f1cdaf
José Andrés Ayestarán <ja.ayestaran@gmail.com>
2013-02-08 00:35:23 -0300

    Agrego algunos cambios y nuevas cosas a forms.py y admin.py

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from usuarios.forms import UsuarioForm, UserChangeForm
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+class MyUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UsuarioForm
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
 
 User = get_user_model()
-admin.site.register(User)
+admin.site.register(User, MyUserAdmin)
+admin.site.unregister(Group)
