cffb5f2e162f3bf3f91a1377fc7af45aedecfcdc
jaayesta <ja.ayestaran@gmail.com>
2013-02-08 01:11:41 -0200

    Update usuarios/admin.py

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+MyUserAdmin(UserAdmin):
+  add_form = UserCreationForm
+
 
 User = get_user_model()
-admin.site.register(User)
+admin.site.register(User, MyUserAdmin)
